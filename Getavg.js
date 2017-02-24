aaa799f8c9990b964bfffecaf353ffc5ac5cd456
JQDOM <Seattlepowers@gmail.com>
2017-02-23 20:09:49 -0800

    adding 2nd easy

---
+++ Getavg.js
@@ -0,0 +1,11 @@
+var arr= [2,10,3];
+
+function avg(arr){
+  var sum = 0;
+  var length= arr.length;
+  for( i= 0; i < length; i++){
+    sum += length[i];
+  }
+  avg=sum/length;
+    console.log("average is " + avg);
+}
